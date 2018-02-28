be035503630672055ee41c9f245a22e9874809f4
Aidar Mamytov <mamytov1905@gmail.com>
2018-02-28 10:03:35 +0600

    Request db and model

---
+++ database/migrations/2018_02_23_165018_create_requests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('contract_id');
+            $table->string('hirer_id');
+            $table->string('hirer_name');
+            $table->string('hirer_email');
+            $table->string('freelancer_id');
+            $table->string('freelancer_name');
+            $table->string('freelancer_email');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
